38042d759881b3e1c6c63157b71b33f9385be69e
megha-56 <meghapanthi435@gmail.com>
2026-03-11 18:36:27 +0530

    React Router

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Footer from './components/footer/footer'
+import Home from './components/Home/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <header/>
+      <Home/>
+      |<Footer/> */}
+
+
+    </>
+  )
+}
+
+export default App
